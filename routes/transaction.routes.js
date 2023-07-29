db6baa24ce53337a403025bc90688c87171e009f
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-07-29 17:02:02 +0200

    transaction routes ok

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 
+const Item = require("../models/Item.model");
+
 const Transaction = require("../models/Transaction.model");
 
 //middleware JWT🛡️
@@ -13,7 +15,6 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 // POST /transactions
 // {
-//   user: "9876", // A ENLEVER
 //   items: ["1234", "2345"] // (1er = le mien, 2e = celui que j'accepte)
 // }
 
@@ -23,17 +24,39 @@ router.post("/transactions", isAuthenticated, function (req, res, next) {
   // 1. que le user connecte possede l'item "1234": que tu check que son .user EST le user connecte
   // 2. que "2345" fait parti des objets proposes: .proposedItems
 
-  if (req.payload._id === req.body.user) {
-    Transaction.create({
-      items: req.body.items,
-    })
-      .then(function (transactionFromDB) {
-        res.status(201).json(transactionFromDB);
-      })
-      .catch((err) => next(err));
-  } else {
-    res.status(401).json({ message: "Unautorized" });
-  }
+  const itemId = req.body.items[0];
+  const proposedItemId = req.body.items[1];
+  console.log(itemId);
+
+  //0
+  Item.findById(itemId).then((item) => {
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    console.log("item ===", item);
+    //1
+    if (req.payload._id === item.user.toString()) {
+      //2
+      if (item.proposedItems.includes(proposedItemId)) {
+        Transaction.create({
+          items: req.body.items,
+        })
+          .then(function (transactionFromDB) {
+            res.status(201).json(transactionFromDB);
+          })
+          .catch((err) => next(err));
+      } else {
+        res.status(403).json({
+          message: `Unautorized: item[1] ${proposedItemId} not proposed`,
+        });
+      }
+    } else {
+      res
+        .status(403)
+        .json({ message: `Unautorized: this item[0] ${itemId} is not yours ` });
+    }
+  });
 });
 
 //READ
